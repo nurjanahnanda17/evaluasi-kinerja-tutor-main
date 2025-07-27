a48b74a9c7e367befae0e37aef41c568a871b4fe
nurjanahnanda17 <nurjanahnanda17@gmail.com>
2025-07-27 12:49:00 +0700

    Fix Evaluasi

--- app/Http/Controllers/EvaluasiController.php
+++ app/Http/Controllers/EvaluasiController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Exports\EvaluasiExport;
-use App\Models\evaluasi;
+use App\Models\Evaluasi;
 use Illuminate\Http\Request;
 use Maatwebsite\Excel\Facades\Excel;
 
